6e62acf7d370b4628848e51b1cacca2ec2758e92
Meda1301 <delphinemedo@gmail.com>
2025-09-23 20:19:19 +0530

    Add files via upload
    Week1

---
+++ day2/Palindrome.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int input = 12345, output=0;
+		
+		int a = input;
+		for (int i=input; i>0; i=i/10)
+		{
+			int rem =input%10;
+			output = output*10 + rem;
+			input =input/10;
+		}
+
+		if(input == output)
+		{
+			System.out.println(a + " is a palindrome");
+		}
+		
+		else
+		{
+			System.out.println(a + " is not a palindrome");
+		}
+		
+	}
+	
+
+}
